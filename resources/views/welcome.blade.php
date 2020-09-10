cdefeee267f0994c5886688707a16bcabbbb7b4c
ELHADJI <damediagnea@gmail.com>
2020-09-10 12:53:34 +0200

    Commit de mon projet Application multi interface

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Home')
+
+@section('content_header')
+    <h1>Clients</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
